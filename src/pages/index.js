eb0f57b871ab127e8020f475adddf139e22c0138
Marques <marques.johnson1992@gmail.com>
2019-11-12 14:43:49 -0500

    navbar built

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 
 import { FaGulp } from 'react-icons/fa';
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
